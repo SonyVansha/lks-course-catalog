d2a91383d931b6a68f94193288b4a8449ba14e35
SonyVansha <sonyvansha@gmail.com>
2025-02-06 09:33:54 +0700

    fix: add sesion token in bucket s3

--- middlewares/uploadFiles.js
+++ middlewares/uploadFiles.js
@@ -7,6 +7,7 @@ const aws_s3 = new S3Client({
    credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY,
       secretAccessKey: process.env.AWS_SECRET_KEY,
+      sessionToken: process.env.AWS_SESSION_TOKEN,
    },
    region: process.env.AWS_BUCKET_REGION,
 });
